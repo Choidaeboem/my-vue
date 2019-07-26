c941cbc98a93f53b00e1d86dfbb42aacb655c6bf
ChoeDaeBeom <chleoqja0125@nate.com>
2019-07-27 07:04:42 +0900

    Good enough

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-app-bar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Hello?</span>
+        <span>Good enough?</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
